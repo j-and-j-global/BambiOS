1e817f8fbc90609ac00b762877cbefc9ef82d314
jspc <james@zero-internet.org.uk>
2019-08-21 21:48:41 +0100

    Update inb_p/outb_p

--- hardware/cmos.c
+++ hardware/cmos.c
@@ -12,22 +12,19 @@ of sample code at https://wiki.osdev.org/CMOS
 /**
    The following two functions (outb_p, inb_p) come from /usr/include/sys/io.h on my build machine:
 
-   Linux seoul-flower 5.2.5-arch1-1-ARCH #1 SMP PREEMPT Wed Jul 31 08:30:34 UTC 2019 x86_64 GNU/Linux
+   Linux seoul-flower 5.2.9-arch1-1-ARCH #1 SMP PREEMPT Fri Aug 16 11:29:43 UTC 2019 x86_64 GNU/Linux
 **/
-static __inline void
-outb_p (unsigned char __value, unsigned short int __port)
-{
-  __asm__ __volatile__ ("outb %b0,%w1\noutb %%al,$0x80": :"a" (__value),
-                        "Nd" (__port));
-}
 
-static __inline unsigned char
-inb_p (unsigned short int __port)
-{
+unsigned char inb_p (unsigned short int __port) {
   unsigned char _v;
 
   __asm__ __volatile__ ("inb %w1,%0\noutb %%al,$0x80":"=a" (_v):"Nd" (__port));
   return _v;
+}
+
+void outb_p (unsigned char __value, unsigned short int __port) {
+  __asm__ __volatile__ ("outb %b0,%w1\noutb %%al,$0x80": :"a" (__value),
+                        "Nd" (__port));
 }
 
 unsigned char read_from_cmos(char index) {
